25500459a0c536db59499282068a93caa5772885
Simon Galli <simon.galli@miromico.ch>
2021-08-06 16:45:10 +0200

    Add multi data import

--- tb_import/cli.py
+++ tb_import/cli.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pathlib import Path
 from typing import List
 import time
+from types import SimpleNamespace
 
 app = typer.Typer()
 
@@ -13,6 +14,15 @@ options = {
     "time_per_batch_ms": 100,
     "verbose": False,
 }
+
+multi_dict = {
+    'data': 'M_MeasurementValue',
+    'key': 'M_MediumID',
+    'timestamp': 'Unixtimestamp',
+    'access_token': 'M_DevEUI',
+}
+
+multi_keys = SimpleNamespace(**multi_dict)
 
 
 @app.command()
@@ -81,10 +91,76 @@ def csv(
         if options["verbose"]:
             typer.echo(f"Status code: {r.status_code}")
 
+@app.command()
+def multi(
+    csvfile: Path = typer.Argument(..., help="Path to the csv file to import the data from"),
+    baseurl: str = typer.Option("https://tesenso.io", help="The thingsboard baseurl with port"),
+    separator: str = typer.Option(";", help="The separator of the csv file"),
+):
+    f"""
+    Add data from a csv file to a single device.
+    Expects the csv file to have a header in the first row.
+    The csv file needs the following keys:
+        {multi_keys.data}: The actual value to be imported into thingsboard
+        {multi_keys.key}: The name of the telemetry key
+        {multi_keys.timestamp}: A unix timestamp in seconds
+        {multi_keys.access_token}: The thingsboard access token of the device
+    """
+    # Get basic variables
+    headers = {"Content-Type": "application/json"}
+
+    try:
+        df = pd.read_csv(csvfile, sep=separator)
+    except FileNotFoundError:
+        typer.echo(f"Could not find csv file {csvfile}!")
+        raise typer.Exit(1)
+
+    if options["verbose"]:
+        typer.echo(f"Read csv file: \n{df}")
+
+    needed_keys = [multi_dict[key] for key in multi_dict.keys()]
+    missing_keys = [key for key in needed_keys if key not in df.keys()]
+
+    if len(missing_keys) > 0:
+        typer.echo(f"Keys {missing_keys} are missing in {csvfile}!")
+        raise typer.Exit(1)
+
+    # separate dataframe into separate access_tokens
+    tokens = df[multi_keys.access_token].unique()
+
+    # loop over all tokens, filter dataframe and create payload with data
+    for token in tokens:
+        url = baseurl.strip("/") + "/api/v1/" + token + "/telemetry"
+        data = df[df[multi_keys.access_token] == token]
+
+        list = []
+        count = 0
+        for index, row in data.iterrows():
+            typer.echo(row)
+            datapoint = {"ts": int(row[multi_keys.timestamp])*1000, "values": {row[multi_keys.key]: row[multi_keys.data]}}
+            list.append(datapoint)
+            count = count + 1
+            if count >= options["entry_per_batch"]:
+                if options["verbose"]:
+                    typer.echo(f"Upload data: {list}")
+                r = requests.post(url=url, headers=headers, data=json.dumps(list))
+                if options["verbose"]:
+                    typer.echo(f"Status code: {r.status_code}")
+                count = 0
+                list = []
+                time.sleep(options["time_per_batch_ms"]/1000)
+
+        # upload last entries
+        if len(list) > 0:
+            if options["verbose"]:
+                typer.echo(f"Upload data: {list}")
+            r = requests.post(url=url, headers=headers, data=json.dumps(list))
+            if options["verbose"]:
+                typer.echo(f"Status code: {r.status_code}")
 
 @app.callback()
 def global_options(
-    entry_per_batch: int = typer.Option(10, help="Amount of datapoints written in a single request"),
+    entry_per_batch: int = typer.Option(100, help="Amount of datapoints written in a single request"),
     time_per_batch_ms: int = typer.Option(100, help="Time to wait between requests in milliseconds"),
     verbose: bool = typer.Option(False, help="Print verbose output"),
 ):
